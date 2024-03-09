943718b2ac931a963c7804db87d7cf594e379381
Bhavani Ravi <bhava0895@gmail.com>
2024-03-09 12:02:05 +0530

    chore: example video 1

--- api.py
+++ api.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI
-from tasks import add_task
+from tasks import add as add_task
 from celery.result import AsyncResult
-import time
+
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
+def hello_world():
     return {"Hello": "World"}
 
 
 @app.get("/tasks/add")
-async def add(x: int, y: int):
-    return {"task_id", add_task.delay(x, y).id}
-    
+def add(x: int, y: int):
+    return {"task_id": add_task.delay(x, y).id}
+
 
 @app.get("/tasks/result")
-async def task_result(task_id):
-    res = AsyncResult(task_id)
-    return {"status": res.ready()}
+def task_status(task_id: str):
+    res = AsyncResult(task_id).ready()
+    return {"task_id": task_id, "result": res}
--- tasks.py
+++ tasks.py
@@ -1,14 +1,17 @@
-from celery import Celery
-from dotenv import load_dotenv
+from celery import Celery, Task
+
 import os
+from dotenv import load_dotenv
 import time
-
-
 load_dotenv()
 
-app = Celery('tasks', broker=os.getenv('CELERY_BROKER_URL'), backend=os.getenv('CELERY_RESULT_BACKEND'))
+app = Celery('tasks', 
+    broker=os.getenv('CELERY_BROKER_URL'), 
+    backend=os.getenv('CELERY_RESULT_BACKEND')
+)
+
 
 @app.task
-def add_task(x, y):
+def add(x, y):
     time.sleep(30)
     return x + y
